3e73b67c7472f1fe61690e2769ac5d9eaa2cc8b5
AniAvazneli <avazneli.ani@gmail.com>
2023-01-12 21:03:27 +0400

    first commit

---
+++ components/Banner.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Image from "next/image"
+
+const Banner = () => {
+  return (
+    <div>
+        {/* <div>
+            <Image/>
+        </div> */}
+    </div>
+  )
+}
+
+export default Banner
